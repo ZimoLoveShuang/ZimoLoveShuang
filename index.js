57de18cf935ceda17700d4a39ea8dd3f7150c124
zimo <461009747@qq.com>
2020-11-05 19:35:45 +0800

    format

--- index.js
+++ index.js
@@ -6,51 +6,51 @@ const xmlreader = require('xmlreader');
 const MUSTACHE_MAIN_DIR = './main.mustache';
 const MAX = 10;
 
+let DATA = {
+    user: {
+        'name': '子墨',
+        'address': '中国，四川，成都',
+        'school': '成都信息工程大学'
+    },
+    date: new Date().toUTCString()
+};
 
-urllib.request('https://blog.zimo.wiki/atom.xml',{
-  timeout: 30 * 1000
+urllib.request('https://blog.zimo.wiki/atom.xml', {
+    timeout: 30 * 1000
 }, function (err, data, res) {
-  if (err) {
-    console.log(err);
-    throw err;
-  }
-
-  xmlreader.read(data.toString(), function(errors, response){
-    if (errors){
-      console.log(errors);
-      throw errors;
+    if (err) {
+        console.log(err);
+        throw err;
     }
 
-    let arts = []
-    for (var i = 0;i < MAX;i++) {
-      var article = {}
-      article.title = response.feed.entry.at(i).title.text();
-      article.url = response.feed.entry.at(i).link.attributes().href;
-      arts.push(article);
-    }
+    xmlreader.read(data.toString(), function (errors, response) {
+        if (errors) {
+            console.log(errors);
+            throw errors;
+        }
 
-    let DATA = {
-      user: {
-          'name': '子墨',
-          'address': '中国，四川，成都',
-          'school': '成都信息工程大学'
-      },
-      date: new Date().toUTCString(),
-      articles: arts
-    };
+        let arts = []
+        for (var i = 0; i < MAX; i++) {
+            var article = {}
+            article.title = response.feed.entry.at(i).title.text();
+            article.url = response.feed.entry.at(i).link.attributes().href;
+            arts.push(article);
+        }
 
-    generateReadMe(DATA)
-  });
+        DATA.articles = arts
+
+        generateReadMe(DATA)
+    });
 });
 
 
 function generateReadMe(DATA) {
-  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
-    if (err) {
-      console.log(err);
-      throw err;
-    }
-    const output = Mustache.render(data.toString(), DATA);
-    fs.writeFileSync('README.md', output);
-  });
+    fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+        const output = Mustache.render(data.toString(), DATA);
+        fs.writeFileSync('README.md', output);
+    });
 }
